ac11bed686f1a4491438be79eb47393bf6337ec2
206 <206@academicos.cesde.edu.co>
2022-11-21 12:16:44 -0500

    se crea plantilla base del proyecto

---
+++ src/main/java/org/example/Clases/Jugador.java
@@ -0,0 +1,67 @@
+package org.example.Clases;
+
+public class Jugador {
+
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private int numeroCamisa;
+    private String posicion;
+    private String equipo;
+
+    public Jugador() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad<18){
+            System.out.println("edad invalida");
+        }else {
+            this.edad=edad;
+        }
+
+    }
+
+    public int getNumeroCamisa() {
+        return numeroCamisa;
+    }
+
+    public void setNumeroCamisa(int numeroCamisa) {
+        this.numeroCamisa = numeroCamisa;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,61 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.example.Clases.Jugador;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ;
+        Scanner entradaPorTeclado = new Scanner(System.in);
+        Jugador objetojugador = new Jugador();
+
+        ArrayList<Jugador> jugadores=new ArrayList<Jugador>();
+
+        int opcion = 0;
+
+        System.out.println("CONVOCATORIA JUGADORES");
+        System.out.println("1. Agregar un jugador");
+        System.out.println("2. Buscar un jugador");
+        System.out.println("3. Editar información de un jugador");
+        System.out.println("4. Mostrar convocatoria");
+        System.out.println("5. Salir");
+
+        do {
+            System.out.println("Digita una opcion del menu: ");
+            opcion = entradaPorTeclado.nextInt();
+
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.print("Ingrese el nombre del jugador: ");
+                    objetojugador.setNombre(entradaPorTeclado.next());
+                    System.out.print("Ingrese el apellido del jugador: ");
+                    objetojugador.setApellidos(entradaPorTeclado.next());
+                    System.out.print("Ingrese la edad del jugador: ");
+                    objetojugador.setEdad(entradaPorTeclado.nextInt());
+                    System.out.print("ingrese el numero de la camisa del jugador: ");
+                    objetojugador.setNumeroCamisa(entradaPorTeclado.nextInt());
+                    System.out.print("Ingrese la posicion del jugador: ");
+                    objetojugador.setPosicion(entradaPorTeclado.next());
+                    System.out.print("Ingrese el equipo del jugador: ");
+                    objetojugador.setEquipo(entradaPorTeclado.next());
+                    break;
+                case 2:
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("OPCION INVALIDA");
+            }
+        }
+        while (opcion !=5);
     }
 }
